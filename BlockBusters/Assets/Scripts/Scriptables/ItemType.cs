bfbc68abda97233a82058e6fc32b0ea608cd3224
Umair <umairwasim001@gmail.com>
2023-05-21 03:44:36 +0500

    Clean Item Type SO

--- BlockBusters/Assets/Scripts/Scriptables/ItemType.cs
+++ BlockBusters/Assets/Scripts/Scriptables/ItemType.cs
@@ -13,9 +13,4 @@ public enum ItemCategory
 public class ItemType : ScriptableObject
 {
     public ItemCategory itemCategoryType;
-    public ItemCategory GetItemType() => itemCategoryType;
-
-    public bool isOrdinaryType() => itemCategoryType == ItemCategory.Ordinary;
-    public bool isExplosiveType() => itemCategoryType == ItemCategory.Explosive;
-    public bool isBonusType() => itemCategoryType == ItemCategory.Bonus;
 }
